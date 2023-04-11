89716f28ecbece664d1a86a7020ade46960e038d
Joseph Cheng <59868375+josephchengyx@users.noreply.github.com>
2023-04-11 19:45:19 +0800

    Update paths
    Replace all instances of old directory name 'scripts' with new name 'Hippocampus_Decoding'

--- get_cells.sh
+++ get_cells.sh
@@ -3,5 +3,5 @@ day=$1
 curr=$PWD
 dest="/Volumes/Hippocampus/Data/picasso-misc/$day/session01"
 cd $dest
-find . -type d -name "cell0*" | cut -d "/" -f 2-4 > ~/Documents/neural_decoding/scripts/cell_list.txt
+find . -type d -name "cell0*" | cut -d "/" -f 2-4 > ~/Documents/neural_decoding/Hippocampus_Decoding/cell_list.txt
 cd $curr
